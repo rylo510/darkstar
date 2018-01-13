9e5f3ef7833d4e1dd021506074d6c2b5a237335a
Ryan <ryan.lambert@gmail.com>
2018-01-12 16:42:54 -0800

    Simplify player commands for mog house and auction house

--- scripts/commands/ah.lua
+++ scripts/commands/ah.lua
@@ -1,14 +1,20 @@
 ---------------------------------------------------------------------------------------------------
--- func: ah
--- desc: opens the Auction House menu anywhere in the world
+-- func: auctionhouse
+-- desc: Shows the Auction House menu
 ---------------------------------------------------------------------------------------------------
+package.loaded["scripts/globals/fateshand"] = nil;
+require("scripts/globals/fateshand");
 
 cmdprops =
 {
-    permission = 1,
-    parameters = ""
+    permission = 0,
+    parameters = "iiii"
 };
-
 function onTrigger(player)
+    if ( hasEnmityNearby(player) == 1 ) then
+        player:PrintToPlayer("Cannot execute command while in battle!");
+        return
+    end
+
     player:sendMenu(3);
-end;
+end
